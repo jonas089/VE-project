9f7f8ec2aafd25cac073a90ede17297d2bc53548
jonas089 <jonaspauli089@gmail.com>
2022-10-28 21:59:44 +0200

    change alert style

--- frontend/src/components/Status.jsx
+++ frontend/src/components/Status.jsx
@@ -4,27 +4,25 @@ export default class Status extends React.Component {
     super(props);
   }
   render() {
-    let color = 'green';
-    if (this.props.status == 'Casper Signer is Locked'){
-      color = 'red';
-    }
-    return(
-      <div class="mx-auto bg-gradient-to-l from-indigo-400 to-red-700">
-        <div class="flex items-center justify-center px-4 sm:px-0">
-          <div role="alert" id="alert" class={"lg:w-10/12 transition duration-150 ease-in-out bg-" + color + "-500 shadow rounded-md md:flex justify-between items-center top-0 mt-12 mb-8 py-4 px-4"}>
-            <div class="sm:flex items-center">
-              <div class="flex items-end">
-                <div class="mr-2 mt-0.5 sm:mt-0 text-red-800">
-                  <img class="focus:outline-none" src="https://www.simpleimageresizer.com/_uploads/photos/9aac2696/VE_mark_1_22x22.png" alt="VE" />
-                </div>
-                  <p class="mr-2 text-base font-bold text-black">Connection Status</p>
-                </div>
-                <div class="h-1 w-1 bg-red-800 rounded-full mr-2 hidden xl:block"></div>
-                   <p class="text-base text-gray-800">{this.props.status}</p>
-                </div>
-            </div>
+    if(this.props.status == 'Ready - Wallet is Connected'){
+      return(
+        <div class="flex bg-green-100 rounded-lg p-4 mb-4 text-sm text-green-700" role="alert">
+          <svg class="w-5 h-5 inline mr-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd"></path></svg>
+          <div>
+              <span class="font-medium">Success alert!</span> Signer is connected.
+          </div>
         </div>
-      </div>
-    );
+      );
+    }
+    else{
+      return(
+        <div class="flex bg-yellow-100 rounded-lg p-4 mb-4 text-sm text-yellow-700" role="alert">
+          <svg class="w-5 h-5 inline mr-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd"></path></svg>
+          <div>
+            <span class="font-medium">Warning alert!</span> Signer is not connected
+          </div>
+        </div>
+      );
+    }
   }
 }
